a3223b907ca91c3820c0bc1257c6d1fc8beda1eb
K-kibet <charleykibet254@gmail.com>
2022-09-19 15:37:10 +0300

    updated files

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const items = document.querySelectorAll('.item');
 const icon = document.querySelector(".icon");
 
-
-const popupEl = `
-<div class="login-wrap">
-<h2>Login</h2>
-
-<div class="form">
-  <input type="text" placeholder="Username" name="un" />
-  <input type="password" placeholder="Password" name="pw" />
-  <button> Sign in </button>
-  <a href="#"> <p> Don't have an account? Register </p></a>
-</div>
-</div>
-`
-
 icon.onclick = () =>{
     location.href = "./app.html"
-    //open("./app.html")
 }
 
 items.forEach(item => {
@@ -28,27 +13,5 @@ items.forEach(item => {
     })
 })
 
-
-
-const app = document.querySelectorAll("div, a");
-const modeBtn = document.querySelector('.modeBtn');
-
-const toggleMode = () =>{
-    if(localStorage.getItem('mode') === "Dark"){
-          localStorage.setItem('mode', "Light")
-    }else  localStorage.setItem('mode',"Dark")
-}
-
-async function setMode(){  
-    await toggleMode
-    app.forEach(el => {
-        el.classList.toggle(localStorage.getItem('mode'))
-        
-    })
-   
-
-}
-
-modeBtn.onclick = setMode
 
     
--- steam-inspired-game-card-hover-effect-main/script.js
+++
@@ -1,5 +0,0 @@
-// Image credits
-// 1. https://pixabay.com/vectors/boxing-ring-wrestling-wrestler-149840/
-// 2. https://pixabay.com/illustrations/retro-background-the-consignment-4237850/
-// 3. https://pixabay.com/illustrations/fractals-environment-chess-1728594/
-// 4. https://pixabay.com/photos/car-racing-motorsport-racing-car-4394450/
